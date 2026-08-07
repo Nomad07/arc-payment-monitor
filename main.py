f48e55bd69ac139f63273586cc5f580b47038f05
Nomad07 <32042394+Nomad07@users.noreply.github.com>
2026-08-08 00:48:22 +0300

    Add initial wallet checker script

---
+++ main.py
@@ -0,0 +1,13 @@
+def main():
+    print("Arc Wallet Checker")
+    print("------------------")
+
+    address = input("Enter Arc wallet address: ")
+
+    print()
+    print("Wallet address:")
+    print(address)
+
+
+if __name__ == "__main__":
+    main()
